5ce0c2db7cc06f25fa0b481bd0f62cdfdb6c1fc3
coyoteshkw <1944301179@qq.com>
2026-02-03 17:54:15 +0800

    init

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -7,28 +7,43 @@ using System.Security.Permissions;
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618
 
-namespace TestMod;
-
-[BepInPlugin("com.author.testmod", "Test Mod", "0.1.0")]
-sealed class Plugin : BaseUnityPlugin
+namespace RedPolePlantMod;
+[BepInPlugin("com.coyoteshkw.RedPolePlantMod", "RedPolePlantMod", "0.1.0")]
 {
-    public static new ManualLogSource Logger;
-    bool IsInit;
-
-    public void OnEnable()
+    // BepInEx 插件标识
+    // [BepInPlugin("com.yourname.redmimic", "Red Pole Mimic", "1.0.0")]
+	public class RedMimic : BaseUnityPlugin
     {
-        Logger = base.Logger;
-        On.RainWorld.OnModsInit += OnModsInit;
-    }
+        // OnEnable 相当于 Vue 的 mounted()，在 Mod 加载完成时执行
+        public void OnEnable()
+        {
+            // 监听日志，确定 Mod 加载成功
+            Logger.LogInfo("拟态草高亮 Mod 已加载！");
 
-    private void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
-    {
-        orig(self);
+            // 注册钩子 (Hook)
+            // 意思：当游戏要运行 PoleMimic 类的 DrawSprites 方法时，先问问我
+            On.PoleMimic.DrawSprites += PoleMimic_DrawSprites;
+        }
 
-        if (IsInit) return;
-        IsInit = true;
+        // 这是我们的自定义逻辑
+        // orig: 原始的方法（你可以选择调用它，或者完全屏蔽它）
+        // self: 当前这根拟态草的实例（相当于 this）
+        // sLeaser: 负责管理图像精灵的容器
+        private void PoleMimic_DrawSprites(On.PoleMimic.orig_DrawSprites orig, PoleMimic self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+        {
+            // 1. 先让原版游戏把草画出来（计算位置、形状、原本的颜色）
+            orig(self, sLeaser, rCam, timeStacker, camPos);
 
-        // Initialize assets, your mod config, and anything that uses RainWorld here
-        Logger.LogDebug("Hello world!");
+            // 2. 修改颜色
+            // 拟态草主要由 sprites[0] 构成杆子主体
+            if (sLeaser.sprites != null && sLeaser.sprites.Length > 0)
+            {
+                // 将颜色强制设为红色
+                sLeaser.sprites[0].color = Color.red;
+
+                // 进阶：如果你想让它在黑暗中发光（无视光照），取消下面这行的注释
+                // sLeaser.sprites[0].shader = rCam.game.rainWorld.Shaders["Basic"];
+            }
+        }
     }
 }
